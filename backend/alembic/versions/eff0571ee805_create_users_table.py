82b4efb5b02227f8522c6f184bb6f932764c6f3e
WathsalaKJ <sandunikekulawala@gmail.com>
2026-08-27 01:25:16 +0530

    file structure changes

---
+++ backend/alembic/versions/eff0571ee805_create_users_table.py
@@ -0,0 +1,35 @@
+"""create users table
+
+Revision ID: eff0571ee805
+Revises:
+Create Date: 2026-08-26 17:26:40.302942
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = "eff0571ee805"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("hashed_password", sa.String(), nullable=False),
+        sa.Column("created_at", sa.DateTime(timezone=True), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")
